22bb30c5026375964bf6da321a5f8a06ba1881ea
NidiaBenitez <49950444+NidiaBenitez@users.noreply.github.com>
2020-12-04 15:08:24 -0300

    Update pptx-to-pdf.ps1

--- pptx-to-pdf.ps1
+++ pptx-to-pdf.ps1
@@ -1,3 +1,4 @@
+# Batch convert all .pptx files encountered in a folder
 # based on scripts found at https://gist.github.com/mp4096/1a2279ec7b3dfec659f58e378ddd9aee and http://tech.franzone.blog/2019/11/19/how-to-create-a-word-to-pdf-converter-script-in-powershell/
 
 #set directory to the folder which contains the files you wish to convert
@@ -7,7 +8,7 @@ Set-Location -Path C:\Users\User\Documents\pdf_files
 $pptx_app = New-Object -ComObject PowerPoint.Application
 $pptx_app.Visible = $True
 
-# Get all objects of type .pptx? in $documents_path and its subfolders
+# Get all objects of type .pptx? in $documents_path
 echo "Processing $($documents_path)"
 Get-ChildItem -Path $documents_path -Filter *.pptx? | ForEach-Object {
 	echo $_.FullName
